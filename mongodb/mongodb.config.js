5d3888840706a7c992d921635cc2e507fb703fed
codernoyan <coder.noyan@gmail.com>
2023-08-16 13:52:42 +0600

    fix: fix spelling error

--- mongodb/mongodb.config.js
+++ mongodb/mongodb.config.js
@@ -1,6 +1,6 @@
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWROD}@cluster0.ufdxsbo.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.ufdxsbo.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
