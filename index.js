42d878f93e041ddebffd2650e4b136ad70ae6ef2
codernoyan <coder.noyan@gmail.com>
2023-08-16 13:51:21 +0600

    feat: added mongodb configuration

--- index.js
+++ index.js
@@ -3,12 +3,16 @@ const app = express();
 const cors = require('cors');
 require('dotenv').config();
 const morgan = require('morgan');
+const { dbConnect } = require('./mongodb/mongodb.config');
 const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
+
+// run mongodb
+dbConnect();
 
 // default get route
 app.get("/", (req, res) => {
