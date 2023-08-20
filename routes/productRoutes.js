b753f787785fab7fabc4500185aa30b043145830
codernoyan <coder.noyan@gmail.com>
2023-08-20 10:13:01 +0600

    feat: added product schema

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,4 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const productSchema = require('../schemas/productSchema');
+const Product = new mongoose.model("Product", productSchema);
+
+
 
 module.exports = router;
---
+++ schemas/productSchema.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+
+const productSchema = mongoose.Schema({
+  productId: {
+    type: String,
+    required: true,
+  },
+  productName: {
+    type: String,
+    required: true,
+  },
+  supplierName: {
+    type: String,
+    required: true,
+  },
+  productGroup: {
+    type: String,
+  },
+  quantityType: {
+    type: String,
+    enum: ["pieces", "kilograms"],
+  },
+  productType: {
+    type: String,
+    enum: ["chicks", "chicken", "medicine", "feed"],
+  },
+  quantity: {
+    type: Number,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  }
+});
+
+module.exports = productSchema;
